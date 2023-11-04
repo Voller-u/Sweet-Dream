b348f4959d3e0a0d37e00e8d9c98590afb8eb77c
ctcticxh <3574736597@qq.com>
2023-11-04 21:50:39 +0800

    战斗部分
    添加敌人技能

--- Sweet_Dream/Assets/Scripts/Enemy.cs
+++ Sweet_Dream/Assets/Scripts/Enemy.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Enemy : MonoBehaviour
 {
     [Header("基本属性")]
@@ -15,6 +15,7 @@ public class Enemy : MonoBehaviour
     public float curDefence;//当前防御力
 
     public GameObject player;
+    private Rigidbody2D rb;
     
     // Start is called before the first frame update
     protected virtual void Start()
@@ -22,23 +23,33 @@ public class Enemy : MonoBehaviour
         transform.position = new Vector3(transform.position.x + speed*Time.deltaTime,transform.position.y,transform.position.z);
         player = GameObject.FindObjectOfType<Player>().gameObject;
         Init();
+        TestSkill();
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
+        Debug.Log(rb.velocity);
         var offset = (player.transform.position - transform.position).normalized;
-        transform.position = new Vector3(transform.position.x + speed * offset.x * Time.deltaTime,
-        transform.position.y + speed * offset.y * Time.deltaTime,0);
+        // transform.position = new Vector3(transform.position.x + speed * offset.x * Time.deltaTime,
+        // transform.position.y + speed * offset.y * Time.deltaTime,0);
         if(curHealth <= 0){
             Destroy(gameObject);
         }
     }
 
     protected void Init(){
+        rb = GetComponent<Rigidbody2D>();
         curSpeed = speed;
         curHealth = maxHealth;
         curAttack = attackInit;
         curDefence = defenceInit;
+    }
+
+    public void TestSkill(){
+        float timer = 0;
+        DOTween.To(()=>timer,X=>timer=X,1,0.8f).OnUpdate(()=>{
+            rb.AddForce(new Vector2(10,0));
+        });
     }
 }
