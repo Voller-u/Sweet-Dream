995b498f2a63285965d7eb9a0812c5550cd4d10e
ctcticxh <3574736597@qq.com>
2023-11-07 22:37:06 +0800

    辅助部分
    优化概率触发函数

--- Sweet_Dream/Assets/Scripts/Enemy.cs
+++ Sweet_Dream/Assets/Scripts/Enemy.cs
@@ -13,6 +13,7 @@ public class Enemy : MonoBehaviour
     public float curAttack;//当前攻击力
     public float defenceInit;//防御力
     public float curDefence;//当前防御力
+    public float attackRadius;//攻击范围
 
     [Header("技能")]
     public List<Skill> skills = new List<Skill>();
@@ -35,14 +36,14 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     protected virtual void Update()
     {
-        offsetFromPlayer = (player.transform.position - transform.position).normalized;
+        offsetFromPlayer = (player.transform.position - transform.position);
         // transform.position = new Vector3(transform.position.x + speed * offset.x * Time.deltaTime,
         // transform.position.y + speed * offset.y * Time.deltaTime,0);
         if(curHealth <= 0){
             Destroy(gameObject);
         }
         if(!isRealsing){
-            ProbableAction(0.3f,TestSkill);
+            ProbableAction(0.3f,TestSkill,offsetFromPlayer.magnitude < attackRadius);
             Debug.Log(this.name + "技能释放了" + "技能冷却状态：" + isRealsing);
         } 
     }
@@ -56,14 +57,13 @@ public class Enemy : MonoBehaviour
     }
 
     protected void InitSkill(){
-        Skill skill0 = new Skill("冲撞",5f,
-        "niha");
+        Skill skill0 = new Skill("冲撞",5f,"niha");
         skills.Add(skill0);
         Debug.Log(skills[0].skillName);
     }
 
     public void TestSkill(){
-        Vector2 attackDirection = offsetFromPlayer;
+        Vector2 attackDirection = offsetFromPlayer.normalized;
         isRealsing = true;
         float timer = 0;
         DOTween.To(()=>timer,X=>timer=X,1,0.8f).OnUpdate(()=>{
@@ -75,8 +75,14 @@ public class Enemy : MonoBehaviour
     }
 
     public delegate void  ProAction();
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="probability">发生的概率</param>
+    /// <param name="proAction">发生的事件</param>
+    /// <param name="otherRestriction">其他条件是否满足</param>
 
-    public void ProbableAction(float probability,ProAction proAction){
+    public void ProbableAction(float probability,ProAction proAction,bool otherRestriction = true){
         if(Random.Range(0,100) <= probability*100){
             proAction();
         }
